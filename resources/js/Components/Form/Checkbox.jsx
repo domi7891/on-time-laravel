7f4839d870e2e4dfa35a5b1a862b2fe103ad8a47
Dominik Nossal <domi.nossal@gmx.at>
2022-07-22 00:38:05 +0200

    Checkbox Komponente erstellt

--- resources/js/Components/Form/Checkbox.jsx
+++ resources/js/Components/Form/Checkbox.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
-export default function Checkbox({ name, value, handleChange }) {
+export default function Checkbox({
+    id,
+    name,
+    handleChange = () => {},
+    checked = false,
+    type = "checkbox",
+    title = "Placeholder",
+    labelClass = "",
+}) {
     return (
-        <input
-            type="checkbox"
-            name={name}
-            value={value}
-            className="rounded border-gray-300 text-accent-400 shadow-sm focus:border-accent-400 focus:ring focus:ring-accent-400/50 focus:ring-opacity-50"
-            onChange={(e) => handleChange(e)}
-        />
+        <label htmlFor={id} className="pr-4 flex items-center gap-3">
+            <div className="flex items-center h-5">
+                <input
+                    id={id}
+                    name={name}
+                    defaultChecked={checked}
+                    onChange={(e) => toggleEqp("USB", e.target.checked)}
+                    type={type}
+                    className="focus:border-solid focus:border focus:border-accent-400/50 focus:ring focus:ring-accent-400 focus:ring-opacity-25 h-4 w-4 text-accent-400 border-gray-300 rounded-full"
+                />
+            </div>
+            <div className="text-left flex-1">
+                <span>{title}</span>
+            </div>
+        </label>
     );
 }
